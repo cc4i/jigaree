9abe41a03b7e3c75e7c2c48c781f9379a9f0538b
Chuan <chuancc@google.com>
2022-03-08 10:44:55 +0800

    added swagger support

--- src/gate/main.go
+++ src/gate/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"gate/log"
+	"net/http"
+
+	docs "gate/docs"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func pHeaders() gin.HandlerFunc {
@@ -17,8 +22,32 @@ func pHeaders() gin.HandlerFunc {
 	}
 
 }
-func router(ctx context.Context, r *gin.Engine) *gin.Engine {
 
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
+func Helloworld(g *gin.Context) {
+	g.JSON(http.StatusOK, "helloworld")
+}
+
+func router(ctx context.Context, r *gin.Engine) *gin.Engine {
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/example")
+		{
+			eg.GET("/helloworld", Helloworld)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	return r
 }
 
