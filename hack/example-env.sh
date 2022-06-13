67c992bba6c3493179433350b6b95bcbbbbdd4aa
Chuan <chuancc@google.com>
2022-06-13 14:09:10 +0800

    update hack scripts

---
+++ hack/example-env.sh
@@ -0,0 +1,9 @@
+# Project ID to host your cluster
+export PROJECT_ID=play-with-anthos-340801
+
+# Which region to place your cluster
+export REGION=asia-east2
+export ZONE=asia-east2-b
+
+# Cluster name of your GKE
+export CLUSTER=jigaree-k8s-cluster
