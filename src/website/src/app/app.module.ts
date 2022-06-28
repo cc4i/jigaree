5d0eb191972a442457f57d6ef3f1feff1f6843e5
Chuan <chuancc@google.com>
2022-06-28 11:51:48 +0800

    update ui for website

--- src/website/src/app/app.module.ts
+++ src/website/src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule,
+    MatRadioModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
